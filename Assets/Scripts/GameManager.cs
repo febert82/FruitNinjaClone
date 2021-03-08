b9c94747a9e4dafba6d7a935235bfa837b6cd811
febert82 <febert82@gmail.com>
2021-03-08 15:51:43 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Score Elements")]
+    public int score;
+    public int highscore;
+    public Text scoreText;
+    public Text highscoreText;
+
+    [Header("Game Over")]
+    public GameObject gameOverPanel;
+    public Text gameOverPanelScoreText;
+    public Text gameOverPanelHighscoreText;
+
+    [Header("Sounds")]
+    public AudioClip[] sliceSounds;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        Advertisement.Initialize("3868887");
+        audioSource = GetComponent<AudioSource>();
+        gameOverPanel.SetActive(false);
+        GetHighscore();
+    }
+
+    private void GetHighscore()
+    {
+        highscore = PlayerPrefs.GetInt("Highscore");
+        highscoreText.text = "Best: " + highscore;
+    }
+
+    public void IncreaseScore(int points)
+    {
+        score += points;
+        scoreText.text = score.ToString();
+
+        if (score > highscore)
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+            highscoreText.text = "Best: " + score.ToString();
+        }
+    }
+
+    public void OnBombHit()
+    {
+        Advertisement.Show();
+        gameOverPanel.SetActive(true);
+        gameOverPanelScoreText.text = "Score: " + score.ToString();
+        gameOverPanelHighscoreText.text = "Best: " + highscore.ToString();
+
+        Time.timeScale = 0;
+        Debug.Log("Bomb hit");
+    }
+
+    public void RestartGame()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOverPanel.SetActive(false);
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Interactable"))
+        {
+            Destroy(g);
+        }
+
+        Time.timeScale = 1;
+    }
+
+    public void PlayRandomSliceSound()
+    {
+        AudioClip randomSound = sliceSounds[Random.RandomRange(0, sliceSounds.Length)];
+        audioSource.PlayOneShot(randomSound);
+    }
+}
